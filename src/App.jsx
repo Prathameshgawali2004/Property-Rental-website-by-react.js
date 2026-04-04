c72722af93009118cd02533183469ec1311572aa
prath <prathameshgawli37@gmail.com>
2026-04-04 11:19:58 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,4 @@
-// import Navbar from "./Component/Navbar/Navbar";
-// import { Outlet } from "react-router-dom";
-// import Footer from"./Component/Footer/Footer";
-
-
-// function App() {
-//   return (
-//     <div>
-//       <Navbar />
-//       <Outlet />
-//       <Footer /> 
-//     </div>
-//   );
-// }
-
-
-// export default App;
-
-
-  import Navbar from "./Component/Navbar/Navbar";
+import Navbar from "./Component/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Component/Footer/Footer";
 import "./App.css";
